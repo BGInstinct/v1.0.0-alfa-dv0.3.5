b301fdf04b9902c810be566d0bde1414c874ca37
BGInstinct <117728987+BGInstinct@users.noreply.github.com>
2024-05-27 09:38:10 +0300

    v1.0.0

---
+++ javascript.js
@@ -0,0 +1,22 @@
+var coll = document.getElementsByClassName("burger-menu");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
+function chall_pdf() {
+  window.location.href = "https://github.com";
+}
+
+function chall_repo() {
+  window.location.href = "https://github.com";
+}
